145b52260412cecc1369767c19ae0ca584601030
manny <emmanuelgomezrealpro@gmail.com>
2023-07-01 13:47:32 -0400

    defining tag

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -5,9 +5,31 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
+  
+  id: {
+    type:DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
     // define columns
   },
+  tag_name:{
+  type: DataTypes.STRING,
+  },
+  productTag.init(
+    id:{
+      type:DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+    },
+    product_id:{
+      type:DataTypes.INTEGER,
+    },
+    tag_id:{
+      type:DataTypes.INTEGER,
+    }
+  )
   {
     sequelize,
     timestamps: false,
